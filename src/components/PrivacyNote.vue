7310b319a3db3ff6f75a5a5101094d197411d4c9
subinamathew <25580800+subinamathew@users.noreply.github.com>
2025-09-17 08:43:59 +0000

    chore: added tipp

--- src/components/PrivacyNote.vue
+++ src/components/PrivacyNote.vue
@@ -5,6 +5,8 @@
       Deine Ergebnisse sind privat und werden nur auf deinem Gerät angezeigt. Wir speichern sie nicht und können sie nicht einsehen.
       <br/><br/>
       Wenn du die Seite verlässt oder neu lädst, gehen die Ergebnisse verloren. Nutze die Funktion "Drucken / Als PDF speichern", um sie zu behalten.
+      <br/><br/>
+      Tipp: Du kannst diese Seite in deine Sprache übersetzen. In Browsern wie Chrome oder Safari findest du dafür oft eine eingebaute Funktion.
     </v-card-text>
   </v-card>
 </template>
